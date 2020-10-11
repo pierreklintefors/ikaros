f5e60356dcea1e3080fc0e9c85b7a2a09feee9dc
pierreklintefors <bev15pkl@student.lu.se>
2020-10-11 15:58:36 +0200

    VocalReaction module added

---
+++ Source/Modules/RobotModules/VocalReaction/VocalReaction.cc
@@ -0,0 +1,156 @@
+//
+//	VocalReaction.cc		This file is a part of the IKAROS project
+//
+//    Copyright (C) 2012 <Author Name>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    See http://www.ikaros-project.org/ for more information.
+//
+//  This example is intended as a starting point for writing new Ikaros modules
+//  The example includes most of the calls that you may want to use in a module.
+//  If you prefer to start with a clean example, use he module MinimalModule instead.
+//
+
+#include "VocalReaction.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// use the ikaros namespace to access the math library
+// this is preferred to using <cmath>
+
+using namespace ikaros;
+
+
+void
+VocalReaction::Init()
+{
+    // The parameters are initialized
+    // from the IKC and can optionally be changed from the
+    // user interface while Ikaros is running. If the parameter is not
+    // set, the default value specified in the ikc-file will be used instead.
+
+    //Sets value of int varibles num_categories number_sound_categories, num_intensity_levels according to values of parameters inputs in user interface
+    Bind(num_categories, "number_sound_categories");
+    Bind (num_sounds_per_category, "sounds_per_category");
+    Bind(num_intensity_levels, "intensity_levels");
+
+
+    io(input_Pos_matrix, input_Pos_matrix_size_x, input_Pos_matrix_size_y, "POSITION_INPUT");
+
+    // Get pointer to a matrix and treat it as a matrix. If an array is
+    // connected to this input, size_y will be 1.
+
+    io(input_ID_matrix, input_ID_matrix_size_x, input_ID_matrix_size_y, "OBJECT_INPUT");
+
+    // Do the same for the outputs
+
+    io(reaction_output_array, reaction_output_array_size, "REACTION_OUTPUT");
+
+
+    // Allocate some data structures to use internaly
+    // in the module
+
+    // Create an array with elements corresponding to number of sounds in the soundlibrary
+    // To access the array use internal_array[i].
+
+    internal_array = create_array(num_categories * num_sounds_per_category * num_intensity_levels);
+
+    // Create a matrix with the same size as POSITION_INPUT
+    // IMPORTANT: For the matrix the sizes are given in order X, Y
+    // in all functions including when the matrix is created
+    // See: http://www.ikaros-project.org/articles/2007/datastructures/
+
+
+}
+
+VocalReaction::~VocalReaction()
+{
+    // In general, a destructor is only necessary
+    // when a module communicates with external devices etc
+    // All modules are destroyed when Ikaros stops
+
+    // Destroy data structures that you allocated in Init.
+
+    destroy_array(internal_array);
+    destroy_matrix(internal_matrix);
+
+    // Do NOT destroy data structures that you got from the
+    // kernel with io or GetInputArray, GetInputMatrix etc.
+}
+
+//Function that generates a random number between a specific range
+int
+VocalReaction::Random(int min, int max) //range : [min, max]
+{
+   static bool first = true;
+   if (first)
+   {
+      float seed = std::time(NULL);
+      srand(seed); //seeding for the first time only!
+      first = false;
+   }
+   return min + rand() % (( max + 1 ) - min);
+}
+
+void
+VocalReaction::Tick()
+{
+
+  //Sets all elements in reaction_output_array to 0
+  for (int i = 0; i < reaction_output_array_size; i++)
+    reaction_output_array[i] = 0;
+
+  //Search the input matrix to see if a marker has been identified
+  for (int j = 0; j < input_ID_matrix_size_y; j++)
+    for (int i = 0; i < input_ID_matrix_size_x; i++)
+    {
+      if (input_ID_matrix[j][i] > 0)
+      {
+        //Sets lower and higher boundary for randomizing a sound in the identified category
+        int lower_bound = input_ID_matrix[j][i] * num_sounds_per_category - num_sounds_per_category;
+        int upper_bound = lower_bound + num_sounds_per_category;
+
+        //Generate a random number between boundaries
+        int rand_num = Random(lower_bound, upper_bound);
+
+        reaction_output_array[rand_num] = 1;
+      }
+    }
+
+    std::cout << "Output:" << "\n";
+    for (int i = 0; i < reaction_output_array_size; i++)
+    {
+      std::cout << reaction_output_array[i];
+    }
+    std::cout << '\n';
+
+  
+
+
+
+
+
+
+}
+
+
+
+// Install the module. This code is executed during start-up.
+
+static InitClass init("VocalReaction", &VocalReaction::Create, "Source/Modules/RobotModules/VocalReaction/");
---
+++ Source/Modules/RobotModules/VocalReaction/VocalReaction.h
@@ -0,0 +1,76 @@
+//
+//	VocalReaction.h		This file is a part of the IKAROS project
+//
+//    Copyright (C) 2012 <Author Name>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    See http://www.ikaros-project.org/ for more information.
+//
+
+#ifndef VocalReaction_
+#define VocalReaction_
+
+#include "IKAROS.h"
+
+class VocalReaction: public Module
+{
+public:
+    static Module * Create(Parameter * p) { return new VocalReaction(p); }
+
+    VocalReaction(Parameter * p) : Module(p) {}
+    virtual ~VocalReaction();
+
+    void 		Init();
+    void 		Tick();
+
+    int     Random(int,int);
+
+    // pointers to inputs and outputs
+    // and integers to represent their sizes
+
+    float *     input_array;
+    int         input_array_size;
+
+    float **    input_Pos_matrix;
+    int         input_Pos_matrix_size_x;
+    int         input_Pos_matrix_size_y;
+
+    float **    input_ID_matrix;
+    int         input_ID_matrix_size_x;
+    int         input_ID_matrix_size_y;
+
+    float *     reaction_output_array;
+    int         reaction_output_array_size;
+
+    float **    output_matrix;
+    int         output_matrix_size_x;
+    int         output_matrix_size_y;
+
+    // internal data storage
+
+    float *     internal_array;
+    float **    internal_matrix;
+
+    // parameter values
+
+    int         num_categories;
+    int         num_sounds_per_category;
+    int         num_intensity_levels;
+
+
+};
+
+#endif
